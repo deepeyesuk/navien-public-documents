bc0692feaa385125d7ba64f1f3ee93a7d6a4ef2c
Youngho Andrew Chaa <andrew.yh.chaa@gmail.com>
2025-11-26 21:47:02 +0000

    Updated for github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://deepeyesuk.github.io',
   base: '/navien-public-documents',
 });
